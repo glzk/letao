6e5c0e88f42e6bc6feaf5ea664092207ee6c36a1
glzk <552455953@qq.com>
2018-08-18 22:29:25 +0800

    index的公共部分功能完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -4,22 +4,51 @@ $(function () {
     NProgress.start();
   })
   $(document).ajaxStop(function () {
-    setImmediate(function () {
       NProgress.done();
-    },1000)
   })
   //登录拦截
-  // if (location.href.indexOf('login.html') ===- 1) {
-  //     $.ajax({
-  //       type: 'get',
-  //       url: '/employee/checkRootLogin',
-  //       dataType: 'json',
-  //       success: function (info) {
-  //         console.log(info);
-          
-  //       }
-  //     })
-  // }
-
+  if (location.href.indexOf('login.html') === -1) {
+      $.ajax({
+        type: 'get',
+        url: '/employee/checkRootLogin',
+        dataType: 'json',
+        success: function (info) {
+          console.log(info);
+          if (info.success) {
+            console.log('登录成功'); 
+          }
+          if (info.error) {
+            location.href = "login.html";
+          }
+        }
+      })
+  }
+  //分类切换功能
+  $('.cate').click(function () {
+    $('.nav .child').stop().slideToggle();
+  })
+  //左侧tab栏切换功能
+  $('.icon_menu').click(function () {
+    $('.lt-aside').toggleClass('hidemenu');
+    $('.it-topbar').toggleClass('hidemenu');
+    $('.lt-main').toggleClass('hidemenu');
+  })
+  //弹出模态框
+  $('.icon_logout').click(function () {
+    $('#Modal').modal('show')
+  })
+  //退出功能
+  $('#icon_logout').click(function () {
+    $.ajax({
+      type: 'get',
+      url: '/employee/employeeLogout',
+      dataType: 'json',
+      success: function (info) {
+        if ( info.success ) {
+          location.href = "login.html";
+        }
+      }
+    })
+  })
 
 })
