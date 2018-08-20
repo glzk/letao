924186d933379fcce6facd92f66583db093db67d
glzk <552455953@qq.com>
2018-08-20 20:19:36 +0800

    second所有功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -13,14 +13,14 @@
       },
       dataType: 'json',
       success: function (info) {
-        var htmlStr = template('secondTpl',info);
+        var htmlStr = template('secondTpl', info);
         $('.lt-content tbody').html(htmlStr);
         //分页功能
         $('#paginator').bootstrapPaginator({
-          bootstrapMajorVersion:3,
+          bootstrapMajorVersion: 3,
           currentPage: info.page,
-          totalPages: Math.ceil(info.total/info.size),
-          onPageClicked: function (a,b,c,page) {
+          totalPages: Math.ceil(info.total / info.size),
+          onPageClicked: function (a, b, c, page) {
             currentPage = page;
             console.log(page);
             render()
@@ -43,26 +43,82 @@
       dataType: 'json',
       success: function (info) {
         console.log(info);
-        var htmlStr = template('cateTpl',info);
+        var htmlStr = template('cateTpl', info);
         $('.dropdown ul').html(htmlStr);
       }
     })
   })
   //给下拉列表a添加点击事件
-  $('.dropdown-menu').on('click','a',function () {
+  $('.dropdown-menu').on('click', 'a', function () {
     var txt = $(this).text();
     $('#dropdownMenu1 .s1').text(txt);
+    var id = $(this).data('id');
+    $('input[name="categoryId"]').val(id);
+    $('#form').data('bootstrapValidator').updateStatus("categoryId", "VALID")
   })
   //上传文件图片 实时预览
   $("#fileupload").fileupload({
-    dataType:"json",
+    dataType: "json",
     //e：事件对象
     //data：图片上传后的对象，通过data.result.picAddr可以获取上传后的图片地址
-    done:function (e, data) {
+    done: function (e, data) {
       console.log(data);
       var imgUrl = data.result.picAddr;
-      $('.form-group img').attr("src",imgUrl) ;
+      $('.form-group img').attr("src", imgUrl);
+      $('[name="brandLogo"]').val(imgUrl);
+      $('#form').data('bootstrapValidator').updateStatus("brandLogo", "VALID")
     }
-});
-
+  });
+  //配置表单校验
+  $('#form').bootstrapValidator({
+    excluded:[],
+    feedbackIcons: {
+      valid: 'glyphicon glyphicon-ok',
+      invalid: 'glyphicon glyphicon-remove',
+      validating: 'glyphicon glyphicon-refresh'
+    },
+    fields: {
+      brandName: {
+        validators: {
+          notEmpty: {
+            message: "请输入二级分类名称"
+          }
+        }
+      },
+      categoryId: {
+        validators: {
+          notEmpty: {
+            message: "请选择一级分类"
+          }
+        }
+      },
+      brandLogo: {
+        validators: {
+          notEmpty: {
+            message: "请上传图片"
+          }
+        }
+      }
+    }
+  })
+  //注册校验成功事件
+  $('#form').on('success.form.bv',function (e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'post',
+      url: '/category/addSecondCategory',
+      data: $('#form').serialize(),
+      success: function (info) {
+        console.log(info);
+        if (info.success) {
+          currentPage = 1;
+          render()
+          $('#secondModal').modal('hide');
+          $('#form').data("bootstrapValidator").resetForm(true);
+          $('.s1').text('请选择一级分类');
+          $('#img').attr('src','./images/none.png');
+        }
+      }
+    })
+  })
 })()
